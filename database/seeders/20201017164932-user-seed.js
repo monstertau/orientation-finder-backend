1384accad5bc58cda10f92bda0124c47a1da241d
Trung Dung Vu <trungdungvu172@gmail.com>
2020-10-18 03:21:58 +0700

    update database

--- database/seeders/20201017164932-user-seed.js
+++ database/seeders/20201017164932-user-seed.js
@@ -5,8 +5,8 @@ module.exports = {
     return queryInterface
       .bulkInsert("Users", [
         {
-          name: "test",
-          email: "test@gmail.com",
+          name: "Vu Trung Dung",
+          email: "trungdungvu172@gmail.com",
           password:
             "$2b$10$aYng9iFbAVBuoZJIZ9pu2emQHBpQkvFMvoVOfIl94OJYmzAX61xLK",
           phone: "0977554906",
@@ -15,8 +15,8 @@ module.exports = {
           updatedAt: new Date(),
         },
         {
-          name: "test2",
-          email: "test2@gmail.com",
+          name: "Vu Minh Hoang",
+          email: "minhhoang99@gmail.com",
           password:
             "$2b$10$aYng9iFbAVBuoZJIZ9pu2emQHBpQkvFMvoVOfIl94OJYmzAX61xLK",
           phone: "0977554906",
@@ -30,14 +30,28 @@ module.exports = {
           {
             CategoryId: 1,
             UserId: 1,
-            UserPoint: 6,
+            UserPoint: 25,
             createdAt: new Date(),
             updatedAt: new Date(),
           },
           {
             CategoryId: 2,
             UserId: 1,
-            UserPoint: 4,
+            UserPoint: 20,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            CategoryId: 3,
+            UserId: 1,
+            UserPoint: 10,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            CategoryId: 4,
+            UserId: 1,
+            UserPoint: 5,
             createdAt: new Date(),
             updatedAt: new Date(),
           },
